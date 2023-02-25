cdb605e2eb09c7ec4002af1dc48212ac9e9a9c3e
Anton Bobryshev <101340267+BobryshevAnton@users.noreply.github.com>
2023-02-25 15:20:31 +0100

    Anton part

--- src/components/CountryList/CountryList.jsx
+++ src/components/CountryList/CountryList.jsx
@@ -1,12 +1,18 @@
 import { Grid, GridItem } from 'components';
+import { useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export const CountryList = ({ countries }) => {
+  const location = useLocation();
+
   return (
     <Grid>
       {countries.map(el => {
         return (
           <GridItem key={el.id}>
-            <img src={el.flag} alt={el.country} />
+            <Link state={{ from: location }} to={`/country/${el.id}`}>
+              <img src={el.flag} alt={el.country} />
+            </Link>
           </GridItem>
         );
       })}
--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,10 +1,45 @@
 import { Section, Container, CountryInfo, Loader } from 'components';
+import { useLocation } from 'react-router-dom';
+
+import { useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import { useState } from 'react';
+import { fetchCountry } from 'service/country-service';
 
 export const Country = () => {
+  const [countryInfo, setCountryInfo] = useState({});
+  const location = useLocation();
+
+  const { id } = useParams();
+  useEffect(() => {
+    fetchCountry(id).then(data => setCountryInfo(data));
+  }, [id]);
+
+  const goBackLink = location?.state?.from ?? '/';
+
   return (
     <Section>
       <Container>
-        <h2>Country</h2>
+        <div
+          style={{
+            marginBottom: '60px',
+            color: '#f2f2f2',
+            letterSpacing: '0.06em',
+            textDecoration: 'underline',
+
+            borderColor: 'gray',
+          }}
+        >
+          <Link to={goBackLink}>Back to Countries</Link>
+        </div>
+        <CountryInfo
+          flag={countryInfo.flag}
+          capital={countryInfo.capital}
+          country={countryInfo.countryName}
+          id={countryInfo.id}
+          languages={countryInfo.languages}
+          population={countryInfo.population}
+        />
       </Container>
     </Section>
   );
